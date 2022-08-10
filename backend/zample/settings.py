d670a2d8eb1a4dcf63e44d3e34327cbeb20ca401
Tito Seoane <te.seoane@gmail.com>
2022-08-10 15:20:39 +0200

    remove secrete key from settings

--- backend/zample/settings.py
+++ backend/zample/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ju1i#__cw1wc^9$&@n1(-*qc*i&)!mp2_^n#ad7mr1v$+o$k@*'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
